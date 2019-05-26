90cf2e2201c16d957450fcf18e1a84f4c7a6e34a
scott246 <ndscott32@gmail.com>
2019-05-26 15:03:27 -0400

    added automata creation ability

--- Automata/AutomataSelect.xaml.cs
+++ Automata/AutomataSelect.xaml.cs
@@ -8,34 +8,61 @@ namespace Automata
 	/// </summary>
 	public partial class AutomataSelect : Window
 	{
-		string username = "";
+		readonly string username = "";
 		public AutomataSelect(string user)
 		{
 			InitializeComponent();
-			test.Content = user;
+			UsernameDisplay.Content = user;
 			username = user;
 		}
 
-		private void Logout(object sender, RoutedEventArgs e)
+		//TODO: delete below constructor and change App.xaml to redirect to login screen
+		public AutomataSelect()
 		{
-			int code = DBOps.Update(
-				string.Format(
-				"UPDATE users SET persist=false WHERE uname='{0}';",
-				username));
-			if (code == 0)
-			{
-				new Login().Show();
-				Close();
-			}
-			else
-			{
-				Console.WriteLine("Error logging out. (" + code + ")");
-			}
+			InitializeComponent();
+			UsernameDisplay.Content = "test1";
+			username = "test1";
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
 			new AutomataEdit(username, "test project title").Show();
+			Close();
+		}
+
+		private void NewButton_Click(object sender, RoutedEventArgs e)
+		{
+			AutomataSelectNew asn = new AutomataSelectNew();
+			asn.ShowDialog();
+			string newAutomataName = asn.GetValues("name");
+			string newAutomataDesc = asn.GetValues("desc");
+			var result = DBOps.Insert(
+				string.Format("INSERT INTO automata(automata_name, automata_desc, enabled) VALUES (\"{0}\", \"{1}\", true);", 
+				newAutomataName, 
+				newAutomataDesc));
+			Console.WriteLine(result);
+			new AutomataEdit(username, newAutomataName).Show();
+			Close();
+		}
+
+		private void OpenButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void DeleteButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void ToggleButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void LogoutButton_Click(object sender, RoutedEventArgs e)
+		{
+			new Login().Show();
 			Close();
 		}
 	}
---
+++ Automata/AutomataSelectNew.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Automata
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class AutomataSelectNew : Window
+	{
+		public AutomataSelectNew()
+		{
+			InitializeComponent();
+		}
+
+		public string GetValues(string value)
+		{
+			switch (value)
+			{
+				case "name":
+					return NameBox.Text;
+				case "desc":
+					return DescriptionBox.Text;
+				default:
+					return "";
+			}
+		}
+
+		private void Create_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+	}
+}
